c69918640d4fd3673a716384273743f136ee506f
Nitin Singh <nitin27may@gmail.com>
2025-04-11 20:26:43 -0400

    DI issue fix

---
+++ backend/src/Contact.Infrastructure/Persistence/Repositories/PageOperationMappingRepository.cs
@@ -0,0 +1,17 @@
+using System.Data;
+using System.Threading.Tasks;
+using Contact.Domain.Entities;
+using Contact.Domain.Interfaces;
+using Contact.Infrastructure.Persistence.Helper;
+
+namespace Contact.Infrastructure.Persistence.Repositories
+{
+    public class PageOperationMappingRepository : GenericRepository<PageOperationMapping>, IGenericRepository<PageOperationMapping>
+    {
+        public PageOperationMappingRepository(IDapperHelper dapperHelper) : base(dapperHelper, "Permissions")
+        {
+        }
+
+        // Add any specific methods for PageOperationMapping if needed
+    }
+}
